d87c7dd21698b4c486871b5b54d5672eaaa3fc04
Zachral <soderberg.danne@gmail.com>
2023-05-12 12:57:36 +0200

    built Hand class, add cardStack list to Card class

--- main.py
+++ main.py
@@ -17,10 +17,20 @@ class Card:
         self.victory_points = victory_points
 
     def __str__(self): 
-        return f"Name: {self.card_name}\nColor: {self.color}\nCost: {self.current_cost}\n\
-                Red discount: {self.red_discount}\nBlue discount: {self.blue_discount}\n\
-                Green discount: {self.green_discount}\nOrange discount: {self.orange_discount}\n\
-                Yellow discount: {self.yellow_discount}\nDiscounted card: {self.discounted_card}\n\
-                Discount amount: {self.amount_discounted_card}\nVictory points: {self.victory_points}\n"
+        return f"Name: {self.card_name}\nColor: {self.color}\nCost: {self.current_cost}\nRed discount: {self.red_discount}\nBlue discount: {self.blue_discount}\nGreen discount: {self.green_discount}\nOrange discount: {self.orange_discount}\nYellow discount: {self.yellow_discount}\nDiscounted card: {self.discounted_card}\nDiscount amount: {self.amount_discounted_card}\nVictory points: {self.victory_points}\n"
+    
+    cardStack = []
+
+class Hand: 
+    def __init__(self, red_discount_total = 0, blue_discount_total = 0, green_discount_total = 0,\
+                  orange_discount_total = 0, yellow_discount_total = 0):
+        self.red_discount_total = red_discount_total
+        self.blue_discount_total = blue_discount_total
+        self.green_discount_total = green_discount_total
+        self.orange_discount_total = orange_discount_total
+        self.yellow_discount_total = yellow_discount_total
+
+    cardsInHand = []
+
 
 
