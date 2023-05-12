cb59c9c755d1391ed77ad85495c3dd905dd0d908
Zachral <soderberg.danne@gmail.com>
2023-05-12 14:52:00 +0200

    Modified Hand class to hold total discounts

--- main.py
+++ main.py
@@ -22,13 +22,12 @@ class Card:
         return f"Name: {self.card_name}\nColor: {self.color}\nCost: {self.current_cost}\nRed discount: {self.red_discount}\nBlue discount: {self.blue_discount}\nGreen discount: {self.green_discount}\nOrange discount: {self.orange_discount}\nYellow discount: {self.yellow_discount}\nDiscounted card: {self.discounted_card}\nDiscount amount: {self.amount_discounted_card}\nVictory points: {self.victory_points}\n"
 
 class Hand: 
-    def __init__(self, red_discount_total = 0, blue_discount_total = 0, green_discount_total = 0,\
-                  orange_discount_total = 0, yellow_discount_total = 0):
-        self.red_discount_total = red_discount_total
-        self.blue_discount_total = blue_discount_total
-        self.green_discount_total = green_discount_total
-        self.orange_discount_total = orange_discount_total
-        self.yellow_discount_total = yellow_discount_total
+    
+    red_discount_total = 0
+    blue_discount_total = 0
+    green_discount_total = 0
+    orange_discount_total = 0
+    yellow_discount_total = 0
 
     cardsInHand = []
 
@@ -46,7 +45,12 @@ Card.cardStack.append(Card("Diaspora", "Yellow", 270, 270, 0, 5, 0, 0, 20, None,
 Card.cardStack.append(Card("Wonder of the World", "Blue and Orange", 290, 290, 0, 20, 0, 20, 0, None, 0, 6))  
 
 
+def print_cardstack():
+    for i in range(len(Card.cardStack)):
+        print(Card.cardStack[i]) 
 
-for i in range(len(Card.cardStack)):
-    print(Card.cardStack[i]) 
+
+print_cardstack()
+
+
 
