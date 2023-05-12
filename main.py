96d38b27e88641158a4be5c211c3e1c15a45a9e3
Zachral <soderberg.danne@gmail.com>
2023-05-12 23:46:02 +0200

    built buy_cards_at_index function

--- main.py
+++ main.py
@@ -21,36 +21,75 @@ class Card:
     def __str__(self): 
         return f"Name: {self.card_name}\nColor: {self.color}\nCost: {self.current_cost}\nRed discount: {self.red_discount}\nBlue discount: {self.blue_discount}\nGreen discount: {self.green_discount}\nOrange discount: {self.orange_discount}\nYellow discount: {self.yellow_discount}\nDiscounted card: {self.discounted_card}\nDiscount amount: {self.amount_discounted_card}\nVictory points: {self.victory_points}\n"
 
-class Hand: 
+class Hand(Card): 
     
     red_discount_total = 0
     blue_discount_total = 0
     green_discount_total = 0
     orange_discount_total = 0
     yellow_discount_total = 0
+    points_total = 0
 
     cardsInHand = []
 
 
-Card.cardStack.append(Card("Cloth Making", "Orange", 50, 50, 0, 5, 0, 10, 0, "Naval Warfare", 10, 1))
-Card.cardStack.append(Card("Sculpture", "Blue", 50, 50, 5, 10, 0, 0, 0, "Architecture", 10, 1))
-Card.cardStack.append(Card("Mysticism", "Blue and Yellow", 50, 50, 0, 5, 0, 0, 5, "Monument", 10, 1))  
-Card.cardStack.append(Card("Urbanism", "Red", 50, 50, 10, 0, 5, 0, 0, "Diplomacy", 10, 1))  
-Card.cardStack.append(Card("Architecture", "Blue", 140, 140, 0, 10, 5, 0, 0, "Mining", 20, 3))  
-Card.cardStack.append(Card("Naval Warfare", "Red", 160, 160, 10, 0, 0, 5, 0,"Diaspora", 20, 3))  
-Card.cardStack.append(Card("Monument", "Yellow and Orange", 180, 180, 0, 0, 0, 10, 10, "Wonder of the World", 20, 3))  
-Card.cardStack.append(Card("Mining ", "Orange", 230, 230, 0, 0, 5, 20, 0, None, 0, 6))  
-Card.cardStack.append(Card("Provincial Empire", "Red", 260, 260, 20, 0, 0, 0, 5, None, 0, 6))  
-Card.cardStack.append(Card("Diaspora", "Yellow", 270, 270, 0, 5, 0, 0, 20, None, 0, 6))  
-Card.cardStack.append(Card("Wonder of the World", "Blue and Orange", 290, 290, 0, 20, 0, 20, 0, None, 0, 6))  
+def add_cards_to_cardstack():
+    Card.cardStack.append(Card("Cloth Making", "Orange", 50, 50, 0, 5, 0, 10, 0, "Naval Warfare", 10, 1))
+    Card.cardStack.append(Card("Sculpture", "Blue", 50, 50, 5, 10, 0, 0, 0, "Architecture", 10, 1))
+    Card.cardStack.append(Card("Mysticism", "Blue and Yellow", 50, 50, 0, 5, 0, 0, 5, "Monument", 10, 1))  
+    Card.cardStack.append(Card("Urbanism", "Red", 50, 50, 10, 0, 5, 0, 0, "Diplomacy", 10, 1))  
+    Card.cardStack.append(Card("Architecture", "Blue", 140, 140, 0, 10, 5, 0, 0, "Mining", 20, 3))  
+    Card.cardStack.append(Card("Naval Warfare", "Red", 160, 160, 10, 0, 0, 5, 0,"Diaspora", 20, 3))  
+    Card.cardStack.append(Card("Monument", "Yellow and Orange", 180, 180, 0, 0, 0, 10, 10, "Wonder of the World", 20, 3))  
+    Card.cardStack.append(Card("Mining ", "Orange", 230, 230, 0, 0, 5, 20, 0, None, 0, 6))  
+    Card.cardStack.append(Card("Provincial Empire", "Red", 260, 260, 20, 0, 0, 0, 5, None, 0, 6))  
+    Card.cardStack.append(Card("Diaspora", "Yellow", 270, 270, 0, 5, 0, 0, 20, None, 0, 6))  
+    Card.cardStack.append(Card("Wonder of the World", "Blue and Orange", 290, 290, 0, 20, 0, 20, 0, None, 0, 6))  
 
 
 def print_cardstack():
     for i in range(len(Card.cardStack)):
         print(Card.cardStack[i]) 
 
+def buy_card_at_index(index: int):
+    ##Redo as a for-loop? 
+    Hand.red_discount_total += Card.cardStack[index].red_discount
+    Hand.blue_discount_total += Card.cardStack[index].blue_discount
+    Hand.green_discount_total += Card.cardStack[index].green_discount
+    Hand.orange_discount_total += Card.cardStack[index].orange_discount
+    Hand.yellow_discount_total += Card.cardStack[index].yellow_discount
+    Hand.points_total += Card.cardStack[index].victory_points
 
+    for i in range(len(Card.cardStack)):
+        if Card.cardStack[index].discounted_card == Card.cardStack[i].card_name:
+            Card.cardStack[i].current_cost = Card.cardStack[i].current_cost - Card.cardStack[index].amount_discounted_card
+            break
+    
+    for i in range(len(Card.cardStack)):
+        if Card.cardStack[i].color == "Red":
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - Hand.red_discount_total
+        if Card.cardStack[i].color == "Blue":
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - Hand.blue_discount_total
+        if Card.cardStack[i].color == "Green":
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - Hand.green_discount_total
+        if Card.cardStack[i].color == "Orange":
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - Hand.orange_discount_total
+        if Card.cardStack[i].color == "Yellow":
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - Hand.yellow_discount_total
+        if Card.cardStack[i].color == "Blue and Yellow":
+            highest_discount = Hand.blue_discount_total if Hand.blue_discount_total > Hand.yellow_discount_total else Hand.yellow_discount_total 
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - highest_discount
+        if Card.cardStack[i].color == "Yellow and orange" :
+            highest_discount = Hand.yellow_discount_total if Hand.yellow_discount_total > Hand.orange_discount_total else Hand.orange_discount_total 
+            Card.cardStack[i].current_cost = Card.cardStack[i].original_cost - highest_discount
+
+        Hand.cardsInHand.append(Card.cardStack[index])
+        Card.cardStack.remove(Card.cardStack[index])
+        
+add_cards_to_cardstack()
 print_cardstack()
+index = int(input("Which card do you want to buy?"))
+buy_card_at_index(index)
 
 
 
