a1679e038c49d9ecf2c5e1c897c182d76a37b3a0
Zachral <soderberg.danne@gmail.com>
2023-05-16 16:24:56 +0200

    separated to different files

--- main.py
+++ main.py
@@ -1,45 +1,6 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-class Card:
-
-    def __init__(self, card_name, color, current_cost, original_cost, 
-                 red_discount, blue_discount, green_discount, orange_discount, yellow_discount, 
-                 discounted_card, amount_discounted_card, victory_points):
-        self.card_name = card_name
-        self.color = color
-        self.current_cost = current_cost
-        self.original_cost = original_cost
-        self.red_discount = red_discount
-        self.blue_discount = blue_discount
-        self.green_discount = green_discount
-        self.orange_discount = orange_discount
-        self.yellow_discount = yellow_discount
-        self.discounted_card = discounted_card
-        self.amount_discounted_card = amount_discounted_card
-        self.victory_points = victory_points
-
-    cardStack = []
-    
-    def __str__(self): 
-        return f"Name: {self.card_name}\nColor: {self.color}\nCost: {self.current_cost}\nRed discount: {self.red_discount}\nBlue discount: {self.blue_discount}\nGreen discount: {self.green_discount}\nOrange discount: {self.orange_discount}\nYellow discount: {self.yellow_discount}\nDiscounted card: {self.discounted_card}\nDiscount amount: {self.amount_discounted_card}\nVictory points: {self.victory_points}\n"
-
-class Hand(Card): 
-    
-    red_discount_total = 0
-    blue_discount_total = 0
-    green_discount_total = 0
-    orange_discount_total = 0
-    yellow_discount_total = 0
-    points_total = 0
-
-    cardsInHand = []
-
-    def show_discounts(self):
-        return f"Discounts:\nRed: {self.red_discount_total}  Blue: {self.blue_discount_total}    Green: {self.green_discount_total}  Orange: {self.orange_discount_total}    Yellow: {self.yellow_discount_total}    VP: {self.points_total}"
-
-
+from app import create_app
+from classes import Card, Hand
 
 def add_cards_to_cardstack():
     Card.cardStack.append(Card("Cloth Making", "Orange", 50, 50, 0, 5, 0, 10, 0, "Naval Warfare", 10, 1))
@@ -126,13 +87,17 @@ def buy_card_at_index():
     Hand.cardsInHand.append(Card.cardStack[index])
     Card.cardStack.remove(Card.cardStack[index])
 
+app = create_app()
 @app.route("/")
 def index():
 
     add_cards_to_cardstack()
 
     while(True):
-        return Hand.show_discounts(Hand) + "\nMain menu" + "\nShow cards in hand" + "\n3. Buy a card"
+        return Hand.show_discounts(Hand) + """<form method="post">
+            <p><input type=submit value="Show cards in hand" name=show_hand>
+            <p><input type=submit value=Buy card name=buy_card>
+        </form>"""
         
         """ userChoice = int(input("Chose an option: "))
         if userChoice == 1:
